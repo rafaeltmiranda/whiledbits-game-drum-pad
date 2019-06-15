cfdbca46446f05f3dadb5db2e445bde44899a669
David Ferreira(Home) <davidrsferreira@gmail.com>
2019-06-15 18:06:17 +0100

    Create implementation of padnotes

--- src/org/academiadecodigo/whiledlings/whiledbits/pads/PadsNotes.java
+++ src/org/academiadecodigo/whiledlings/whiledbits/pads/PadsNotes.java
@@ -1,14 +1,16 @@
 package org.academiadecodigo.whiledlings.whiledbits.pads;
 
+import org.academiadecodigo.whiledlings.whiledbits.sound.SoundMechanism;
 import org.academiadecodigo.whiledlings.whiledbits.sound.SoundsGroup;
 
 public class PadsNotes extends Pads{
 
     private SoundsGroup soundsGroup;
+    private SoundMechanism sound;
 
-    public PadsNotes (SoundsGroup soundsGoup) {
+    public PadsNotes (SoundsGroup soundsGroup) {
 
-        this.soundsGroup = soundsGoup;
+        this.soundsGroup = soundsGroup;
 
     }
 
@@ -16,6 +18,58 @@ public class PadsNotes extends Pads{
     @Override
     public void padPressed(int pad) {
 
+        switch (pad) {
+
+            case 0:
+                sound = new SoundMechanism(PathNotes.PAD_0.getPath());
+                sound.play(true);
+                break;
+
+            case 1:
+                sound = new SoundMechanism(PathNotes.PAD_1.getPath());
+                sound.play(true);
+                break;
+
+            case 2:
+                sound = new SoundMechanism(PathNotes.PAD_2.getPath());
+                sound.play(true);
+                break;
+
+            case 3:
+                sound = new SoundMechanism(PathNotes.PAD_3.getPath());
+                sound.play(true);
+                break;
+
+            case 4:
+                sound = new SoundMechanism(PathNotes.PAD_4.getPath());
+                sound.play(true);
+                break;
+
+            case 5:
+                sound = new SoundMechanism(PathNotes.PAD_5.getPath());
+                sound.play(true);
+                break;
+
+            case 6:
+                sound = new SoundMechanism(PathNotes.PAD_6.getPath());
+                sound.play(true);
+                break;
+
+            case 7:
+                sound = new SoundMechanism(PathNotes.PAD_7.getPath());
+                sound.play(true);
+                break;
+
+            case 8:
+                sound = new SoundMechanism(PathNotes.PAD_8.getPath());
+                sound.play(true);
+                break;
+
+            case 9:
+                sound = new SoundMechanism(PathNotes.PAD_9.getPath());
+                sound.play(true);
+                break;
+        }
     }
 
     @Override
@@ -25,6 +79,6 @@ public class PadsNotes extends Pads{
 
     @Override
     public int activePad() {
-        return 0;
+        return -1;
     }
 }
